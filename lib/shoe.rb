eba6eef7d51f70c6ec12fa665ff9ec1508151c04
Jesse Barnes <jessekathrynbarnes@gmail.com>
2019-04-25 03:32:46 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,2 +1,13 @@
 class Shoe
-
+attr_accessor :author, :page_count, :genre
+  attr_reader :brand
+ 
+  def initialize(brand)
+    @brand = brand
+  end
+ 
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+ 
+end
